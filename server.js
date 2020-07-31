1f8c07ba6da031a6e78d88858a3c37272ac38a65
Conrad Grant <cgrant@cytracom.com>
2020-07-31 09:12:10 -0500

    cleanup backend

--- server.js
+++ server.js
@@ -4,81 +4,82 @@ const io = require('socket.io')(http);
 const cherrio = require("cheerio");
 const axios = require('axios');
 const nodemailer = require("nodemailer");
-const env = require('dotenv').config();
+require('dotenv').config();
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
-});
+// app.get('/', (req, res) => {
+//   res.sendFile(__dirname + '/index.html');
+// });
 
-const res = setInterval(apiCall, 25000);
+console.log('Node Starting...');
+
+/**
+ * Query website every minute
+ */
+setInterval(apiCall, 60000);
 let previousItemList = [];
-console.log('Here we go...');
+
 function apiCall() {
-  axios.get('https://stickthison.com/collections/morale-patches').then(urlResponse => {
-  // axios.get('https://www.spiritussystems.com/shop-all/?sort=best-selling&page=1').then(urlResponse => {
-    console.log('Making call to Spiritus');
+  // axios.get('https://stickthison.com/collections/morale-patches').then(urlResponse => {
+    axios.get('https://www.spiritussystems.com/shop-all/?sort=best-selling&page=1').then(urlResponse => {
+    console.log('Spiritus: Making call');
     let currentItemList = [];
     const $ = cherrio.load(urlResponse.data);
-    // $('article.product-item').each((i, element) => {
-      // const productTitle = $(element).find("div.product-item-details h3 a").attr('title')
-      // const alertMessage = $(element).find("div.alert-message").html();
-      // const OOS = $(element).find("div.product-item-details div").hasClass('alert-message')
-      // currentItemList.push({title: productTitle, outOfStock: OOS});
+    $('article.product-item').each((i, element) => {
+    const productTitle = $(element).find("div.product-item-details h3 a").attr('title')
+    const productLink = $(element).find("figure.product-item-thumbnail a").attr('href');
+    const OOS = $(element).find("div.product-item-details div").hasClass('alert-message');
+    currentItemList.push({title: productTitle, outOfStock: OOS, productLink: productLink});
 
+    /**
+     * STICKTHISON SCRAPER
+     */
+    // $('div.one-third.column').each((i, element) => {
+    //   const productTitle = $(element).find("a div.info span.title").text();
+    //   const OOS = $(element).find("a div.info span.price span").hasClass('sold_out')
+    //   currentItemList.push({title: productTitle, outOfStock: OOS, productLink: 'N/A'});
 
-      $('div.one-third.column').each((i, element) => {
-          const productTitle = $(element).find("a div.info span.title").text();
-          const OOS = $(element).find("a div.info span.price span").hasClass('sold_out')
-          currentItemList.push({title: productTitle, outOfStock: OOS});
     })
 
     if (previousItemList.length === 0) {
-      console.log('Starting script');
+      console.log('Spiritus: Nothing previous to compare, script now starting...');
       previousItemList = currentItemList;
-      // return 'first loop wont work';
     } else {
+      /**
+       * If the current array of data does not match previous, then something changed...otherwise nothing changed.
+       */
       let inventoryChange = currentItemList.filter(function (item, index) {
         return !(item.outOfStock === previousItemList[index].outOfStock);
       });
-      // let inventory = currentItemList.filter(function (item, index) {
-      //     return (item.outOfStock === previousItemList[index].outOfStock);
-      // });
 
+      // Nothing changed
       if (inventoryChange.length === 0) {
-        console.log('no change!');
-        // return 'No Change In Inventory!';
-        io.emit('Spiritus Broadcast', `No Change In Inventory`)
-
-        // console.log(inventory);
+        console.log('Spiritus: No change in inventory');
+        io.emit('Spiritus Broadcast', `No Change In Inventory`);
+        // can remove this later just doing this so i get payload of array
+        // main(currentItemList).catch(console.error);
       } else {
-        // console.log(inventoryChange);
-        for (let i = 0; i < inventoryChange.length; i++) {
-          let name = inventoryChange[i].title;
-          let oos = inventoryChange[i].outOfStock;
-          console.log('INVENTORY ALERT: ' + name + ' is now ' + (oos ? 'out of stock :(' : 'IN STOCK -- GOGOGO'));
-          io.emit('Spiritus Broadcast', ('INVENTORY ALERT: ' + name + ' is now ' + (oos ? 'out of stock :(' : 'IN STOCK -- GOGOGO')))
-          main('INVENTORY ALERT: ' + name + ' is now ' + (oos ? 'out of stock :(' : 'IN STOCK -- GOGOGO')).catch(console.error);
+        console.log('Spiritus: Inventory Changed, should be getting an email');
+        // console.log('INVENTORY ALERT: ' + name + ' is now ' + (oos ? 'out of stock :(' : 'IN STOCK -- GOGOGO'));
+        // io.emit('Spiritus Broadcast', ('INVENTORY ALERT: ' + name + ' is now ' + (oos ? 'out of stock :(' : 'IN STOCK -- GOGOGO')))
+        main(inventoryChange).catch(console.error);
 
-          // return ('INVENTORY ALERT: ' + name + ' is now ' + (oos ? 'out of stock :(' : 'IN STOCK -- GOGOGO'));
-        }
+        // main('INVENTORY ALERT: ' + name + ' is now ' + (oos ? 'out of stock :(' : 'IN STOCK -- GOGOGO')).catch(console.error);
         previousItemList = currentItemList;
       }
     }
-
   })
 }
 
 
-
-
-
-
-
 // async..await is not allowed in global scope, must use a wrapper
-async function main(message) {
-  // Generate test SMTP service account from ethereal.email
-  // Only needed if you don't have a real mail account for testing
-  // let testAccount = await nodemailer.createTestAccount();
+async function main(bundle) {
+  let message = '';
+  for (let i = 0; i < bundle.length; i++) {
+    let name = bundle[i].title;
+    let oos = bundle[i].outOfStock;
+    let productLink = bundle[i].productLink;
+    message += ("<b>Product Name: </b>" + name + (oos ? ' OUT OF STOCK :(' : ' IN STOCK') + ' Product Link: ' + productLink + " <br>") ;
+  }
 
   // create reusable transporter object using the default SMTP transport
   let transporter = nodemailer.createTransport({
@@ -88,8 +89,8 @@ async function main(message) {
     secure: false, // true for 465, false for other ports
     auth: {
       // user: 'mallory.rosenbaum@ethereal.email', // generated ethereal user
-      user: process.env.MAIL_UN, // generated ethereal user
       // pass: 'txNRfQjMA2aGzXtqnc', // generated ethereal password
+      user: process.env.MAIL_UN, // generated ethereal user
       pass: process.env.MAIL_PW, // generated ethereal password
     },
   });
@@ -97,29 +98,21 @@ async function main(message) {
   // send mail with defined transport object
   let info = await transporter.sendMail({
     // from: 'mallory.rosenbaum@ethereal.email', // sender address
-    from: 'imovertheinternet@gmail.com', // sender address
-    to: "eurofraid@gmail.com", // list of receivers
-    subject: "Spiritus Inventory Check", // Subject line
-    text: message, // plain text body
-    // text: "Hello world?", // plain text body
-    // html: "<b>Hello world?</b>", // html body
-    html: message, // html body
+    from: 'imovertheinternet@gmail.com',
+    to: "eurofraid@gmail.com",
+    subject: "Spiritus Inventory Update 😍",
+    text: message,
+    html: message,
   });
 
-  console.log("Message sent: %s", info.messageId);
-  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-
-  // Preview only available when sending through an Ethereal account
-  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-  // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+  console.log("Message sent: %s", info.messageId); // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
 }
 
-// main('nomg whatt').catch(console.error);
 
-
-
-
-
+/**
+ * Websock for frontend
+ * TODO Not sure how to best utilize this feature yet
+ */
 io.on('connection', (socket) => {
   console.log('a user connected');
   // console.log('here is res from setinval', res);
@@ -133,6 +126,9 @@ io.on('connection', (socket) => {
     io.emit('my broadcast', `server: ${msg}`)
   });
 });
+
 http.listen(3000, () => {
   console.log('listening on *:3000');
+  // console.log('Mail UN: ', process.env.MAIL_UN);
+  // console.log('Mail UN: ', process.env.MAIL_PW);
 });
